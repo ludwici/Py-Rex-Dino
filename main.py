76b3b079a2d32b4493b0d99ad757df7c3bc356b3
Andrew <gammaclone@gmail.com>
2021-01-22 23:05:56 +0200

    Change bg_color and maximum FPS

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ from DinoScene import DinoScene
 
 
 def main():
-    r = RenderWindow()
+    r = RenderWindow(bg_color=(32, 33, 36))
+    r.fps = 75
     ds = DinoScene("Dino Scene")
     r.start(ds)
 
